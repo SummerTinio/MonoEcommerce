3d14624b19de5db790b440a2f094c262ab337e3d
Summer Tinio <79723484+SummerTinio@users.noreply.github.com>
2022-01-07 07:35:50 -0800

    Add JSON-parsing middleware

--- server/start-server.ts
+++ server/start-server.ts
@@ -11,6 +11,8 @@ import getRoutes from './routes';
 function startServer(handle: NextHandlerType, port: number) {
   const app = express();
 
+  app.use(express.json());
+
   app.use('/api', getRoutes());
 
   app.use(errorHandler);
