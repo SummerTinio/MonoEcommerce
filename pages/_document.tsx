e881a258e64cb68fd31cd20676ffeea1bca0f29d
Summer Tinio <79723484+SummerTinio@users.noreply.github.com>
2022-01-08 23:57:46 -0800

    Add Custom _document to NextJS, provide SC styles

---
+++ pages/_document.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
+import theme from '../styles/theme';
+
+import { NextPageContext } from 'next';
+import { style } from '@mui/system';
+
+interface MyDocumentProps {
+
+}
+
+const MyDocument = function MyDocumentComponent<MyDocumentProps>({ }) {
+  return (
+    <>
+      <Html lang="en">
+        <Head>
+          {/** PWA primary color */}
+          <meta content={theme.palette.primary.main} name="theme-color" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    </>
+  );
+}
+
+MyDocument.getInitialProps = async (ctx: NextPageContext) => {
+  const sheet = new ServerStyleSheet();
+  const originalRenderPage = ctx.renderPage;
+
+  try {
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
+      });
+
+    const initialProps = await Document.getInitialProps(ctx);
+    return {
+      ...initialProps,
+      styles: (
+        <>
+          {initialProps.styles}
+          {sheet.getStyleElement()}
+        </>
+      ),
+    };
+  } finally {
+    // to prevent memory leaks in case custom _document doesn't seal itself
+    sheet.seal();
+  }
+};
+
+export default MyDocument;
